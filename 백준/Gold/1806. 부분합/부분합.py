1df5c427403d027dbc24ac1e24bcf8f25281c960
Changhyun-Hong <48922050+spearStr@users.noreply.github.com>
2024-03-25 22:48:15 +0900

    [Gold IV] Title: 부분합, Time: 144 ms, Memory: 120932 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -7,9 +7,6 @@ section_sum = 0
 answer = n + 1
 while True:
     if section_sum >= s:
-        if right == 0:
-            answer = 1
-            break
         section_sum -= number[left]
         answer = min(answer, right - left)
         left += 1
