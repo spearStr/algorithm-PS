b46b4dd56260be222ce5079012fb86f3fa65f908
Changhyun-Hong <alex963@naver.com>
2025-01-02 21:44:32 +0900

    [Silver IV] Title: 국영수, Time: 396 ms, Memory: 54396 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.js
+++ 백준/Silver/10825. 국영수/국영수.js
@@ -1,16 +1,25 @@
 const fs = require('fs');
-const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+const input = fs.readFileSync("/dev/stdin", 'utf8').split('\n');
+const score = [];
 
 const n = Number(input[0]);
-const inputData = input.slice(1);
 
-const score = inputData.map((line) => line.split(' '));
+for (let i = 1; i <= n; i++) {
+    const [name, korean, english, math] = input[i].split(' ');
+    score.push({
+        name,
+        korean: Number(korean),
+        english: Number(english),
+        math: Number(math)
+    });
+}
 
 score.sort((a, b) => {
-    if (Number(a[1]) !== Number(b[1])) return Number(b[1]) - Number(a[1]);
-    if (Number(a[2]) !== Number(b[2])) return Number(a[2]) - Number(b[2]);
-    if (Number(a[3]) !== Number(b[3])) return Number(b[3]) - Number(a[3]);
-    return a[0] > b[0] ? 1 : -1;
+    if (a.korean !== b.korean) return b.korean - a.korean;
+    if (a.english !== b.english) return a.english - b.english;
+    if (a.math !== b.math) return b.math - a.math;
+
+    return a.name > b.name ? 1 : -1;
 });
 
-console.log(score.map((student) => student[0]).join('\n'));
+console.log(score.map((student) => student.name).join('\n'));
