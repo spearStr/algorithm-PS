0f87022f31f0f6672afdb1abb61b787efc8200d7
Changhyun-Hong <alex963@naver.com>
2025-01-02 13:37:20 +0900

    [Silver IV] Title: 국영수, Time: 3796 ms, Memory: 65760 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.js
+++ 백준/Silver/10825. 국영수/국영수.js
@@ -18,17 +18,8 @@ score.sort((a, b) => {
     if (a.korean !== b.korean) return b.korean - a.korean;
     if (a.english !== b.english) return a.english - b.english;
     if (a.math !== b.math) return b.math - a.math;
-
-    for (let i = 0; i < Math.min(a.name.length, b.name.length); i++) {
-        const charA = a.name.charCodeAt(i);
-        const charB = b.name.charCodeAt(i);
-        
-        if (charA !== charB) {
-            return charA - charB;
-        }
-    }
     
-    return a.name.length - b.name.length;
+    return a.name > b.name ? 1 : -1;
 });
 
 score.forEach((student) => {
