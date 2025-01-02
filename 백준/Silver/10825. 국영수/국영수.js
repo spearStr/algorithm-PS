1bd0c72874c5211b5d687fcc3765501fbf9e41a7
Changhyun-Hong <alex963@naver.com>
2025-01-02 13:50:33 +0900

    [Silver IV] Title: 국영수, Time: 3780 ms, Memory: 66432 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.js
+++ 백준/Silver/10825. 국영수/국영수.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
-const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+const input = fs.readFileSync("/dev/stdin", 'utf8').split('\n');
+const score = [];
 
 const n = Number(input[0]);
-const inputData = input.slice(1);
 
-const score = inputData.map((line) => {
-    const [name, korean, english, math] = line.split(' ');
-    return {
+for (let i = 1; i <= n; i++) {
+    const [name, korean, english, math] = input[i].split(' ');
+    score.push({
         name,
         korean: Number(korean),
         english: Number(english),
-        math: Number(math),
-    };
-});
+        math: Number(math)
+    });
+}
 
 score.sort((a, b) => {
     if (a.korean !== b.korean) return b.korean - a.korean;
     if (a.english !== b.english) return a.english - b.english;
     if (a.math !== b.math) return b.math - a.math;
-    
+
     return a.name > b.name ? 1 : -1;
 });
 
