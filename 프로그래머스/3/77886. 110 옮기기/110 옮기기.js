f13f6ced6efc5b98321c75c36119955429b4f41f
Changhyun-Hong <alex963@naver.com>
2025-01-08 16:46:18 +0900

    [level 3] Title: 110 옮기기, Time: 116.70 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.js
@@ -0,0 +1,42 @@
+function is110(stack) {
+    const size = stack.length
+    if (size < 3) return false
+    
+    return stack[size - 3] === '1' && stack[size - 2] === '1' && stack[size - 1] === '0'
+}
+
+function solution(s) {
+    const answer = []
+    
+    for (word of s) {
+        let count = 0
+        let tempString = ''
+        const stack = []
+        
+        for (const letter of word) {
+            stack.push(letter)
+            if (is110(stack)) {
+                stack.pop()
+                stack.pop()
+                stack.pop()
+                count += 1
+            }
+        }
+        tempString = stack.join("")
+        const lastZeroIdx = tempString.lastIndexOf("0")
+        const add110 = "110".repeat(count)
+
+        if (tempString.length === 0) {
+            answer.push(add110)
+            continue;
+        }
+        
+        if (lastZeroIdx === -1) {
+            answer.push(add110 + tempString)
+            continue;
+        }
+
+        answer.push(tempString.slice(0, lastZeroIdx + 1) + add110 + tempString.slice(lastZeroIdx + 1, tempString.length))
+    }
+    return answer
+}
