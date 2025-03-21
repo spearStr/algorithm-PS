d4b17ac7a5d4335d56ddc55695293cb5ca0464c1
Changhyun-Hong <alex963@naver.com>
2025-03-21 10:53:05 +0900

    [level 4] Title: 징검다리, Time: 43.08 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.js
+++ 프로그래머스/4/43236. 징검다리/징검다리.js
@@ -2,27 +2,28 @@ function solution(distance, rocks, n) {
     rocks.sort((a, b) => a - b);
     rocks.push(distance);
     
-    let left = 1, right = distance;
+    let low = 1
+    let high = distance;
     let answer = 0;
     
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
         let removeCount = 0;
         let prev = 0;
         
         for (let rock of rocks) {
             if (rock - prev < mid) {
-                removeCount++;
+                removeCount += 1;
             } else {
                 prev = rock;
             }
         }
         
         if (removeCount > n) {
-            right = mid - 1;
+            high = mid - 1;
         } else {
             answer = mid;
-            left = mid + 1;
+            low = mid + 1;
         }
     }
     
